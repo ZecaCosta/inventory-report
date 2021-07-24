6e9c4f53cccc72bad572db37fe4fbaed1eb1894c
Zeca Costa <jccostaso@gmail.com>
2021-07-24 19:46:30 -0300

    refact req7

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -1,3 +1,17 @@
-class InventoryIterator:
-    def __iter__(self):
-        pass
+from collections.abc import Iterator
+
+
+class InventoryIterator(Iterator):
+    @classmethod
+    def __init__(cls, iterable_object):
+        cls.iterable_object = iterable_object
+        cls.element = 0
+
+    def __next__(cls):
+        try:
+            data = cls.iterable_object[cls.element]
+        except IndexError:
+            raise StopIteration()
+        else:
+            cls.element += 1
+            return data
